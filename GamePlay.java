e95e1c0b71494c0839281e3b9c5159d3ddb59244
syared <syared@newroads.org>
2017-02-22 12:36:55 -0500

    hello

--- GamePlay.java
+++ GamePlay.java
@@ -7,18 +7,19 @@ public class GamePlay{
       //Trying to set up the game logic
       
       //  Make GamePlay:
-  1) Allow for Player to input x and y values which will be marked with 'x' to signify a ship.
+  // 
+      /*1) Allow for Player to input x and y values which will be marked with 'x' to signify a ship.
   a) enter x1,y1,x2,y2 values
     
   2) Using if else; if value/char is equal to the opponents x1,y1,x2,y2 then print "you hit" (and removed value)
       else return "you missed"
   3) Using if else; if value/char is equal to the x1,y1,x2,y2 (equal to all of these) print "you sunk the ship"
     else return "you missed"
-      
+      */
        
       
       
-     /*System.out.println("Welcome to BattleShip");
+   /*  System.out.println("Welcome to BattleShip");
     
      System.out.println("Choose the placement of your first ship");
   
@@ -44,7 +45,7 @@ public class GamePlay{
        
     else if (System.out.println("You missed"));{
       
-    }*/
+    }
             
  }
                         
